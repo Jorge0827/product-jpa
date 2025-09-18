b86ed8a377396ce82923a2f666be3e3a48b42e8a
Jorge0827 <jorgean0827@gmail.com>
2025-09-18 16:13:35 -0500

    Estructura Incial del proyecto

---
+++ src/main/java/com/jechavarria/products_jpa/ProductsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jechavarria.products_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jechavarria/products_jpa/ProductsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jechavarria.products_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
